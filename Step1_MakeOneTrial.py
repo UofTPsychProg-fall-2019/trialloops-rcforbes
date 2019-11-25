749c138ab2692ecff581b137dcb9f418a29e6de1
rcforbes <rachelchforbes@gmail.com>
2019-11-25 11:27:34 -0500

    RForbes_PS5_FinalSubmission

--- Step1_MakeOneTrial.py
+++ Step1_MakeOneTrial.py
@@ -15,7 +15,22 @@ import os, sys
 from psychopy import visual, core, event, gui, logging
 
 # open a white full screen window
-win = visual.Window(fullscr=True, allowGUI=False, color='white', unit='height') 
+win = visual.Window(fullscr=True, allowGUI=False, color='white', unit='height')
+
+instr = visual.TextStim(win, color = 'black', height=.05, text="""Rachel Forbes PS5 
+Step 1: Make one trial
+
+Hope you like cute pets! In this experiment you will be rating different pets on cuteness.
+
+To make a rating click somewhere on the line. To accept your rating, either press 'enter' or click the glowing button.
+
+Press any key to begin (or escape to quit).""")
+
+event.clearEvents()
+instr.draw()
+win.flip()
+if 'escape' in event.waitKeys():
+    core.quit()
 
 # uncomment if you use a clock. Optional because we didn't cover timing this week, 
 # but you can find examples in the tutorial code 
@@ -28,17 +43,37 @@ win = visual.Window(fullscr=True, allowGUI=False, color='white', unit='height')
 # if you're stuck you can use the responseExercise.py answer as a starting point 
 
 # maybe start by making stimulus objects (e.g. myPic = visual.ImageStim(...))  
+ratingScale = visual.RatingScale(win, textColor = 'black', lineColor = 'black', choices=['cute', 'very cute', 'cutest'], pos=(0, -.45))
+text = visual.TextStim(win, text='How cute is this picture?', color = 'black', pos=(0,-.4), height=.05)
+x, y = ratingScale.win.size
+image1 = visual.ImageStim(win=win, image='Pics/Gizmo.jpg', units='pix',pos=[0, y//15])
 
-# then draw all stimuli
+# then draw all stimuli and flip window
+while ratingScale.noResponse:
+    text.draw()
+    ratingScale.draw()
+    image1.draw()
+    win.flip()
+    if event.getKeys(['escape']):
+        core.quit()
 
-# then flip your window
+core.wait(0.2)
 
 # then record your responses
+data= [ratingScale.getRating(), ratingScale.getRT()]
 
+close = visual.TextStim(win, color = 'black', height=.05, text="""Rachel Forbes PS5
+Step 1: Make one trial
 
+You have rated one pet!
+Press escape to quit. """)
+
+close.draw()
+win.flip()
+if 'escape' in event.waitKeys():
+    core.quit()
 #%% Required clean up
 # this cell will make sure that your window displays for a while and then 
 # closes properly
 
-core.wait(2)
 win.close()
--- Step2_LoopIt.py
+++ Step2_LoopIt.py
@@ -15,7 +15,22 @@ import os, sys
 from psychopy import visual, core, event, gui, logging
 
 # open a white full screen window
-win = visual.Window(fullscr=True, allowGUI=False, color='white', unit='height') 
+win = visual.Window(fullscr=True, allowGUI=False, color='white', unit='height')
+
+instr = visual.TextStim(win, color = 'black', height=.05, text="""Rachel Forbes PS5
+Step 2: Build a trial loop
+
+Hope you like cute pets! In this experiment you will be rating different pets on cuteness.
+
+To make a rating click somewhere on the line. To accept your rating, either press 'enter' or click the glowing button.
+
+Press any key to begin (or escape to quit).""")
+
+event.clearEvents()
+instr.draw()
+win.flip()
+if 'escape' in event.waitKeys():
+    core.quit()
 
 # uncomment if you use a clock. Optional because we didn't cover timing this week, 
 # but you can find examples in the tutorial code 
@@ -25,14 +40,29 @@ win = visual.Window(fullscr=True, allowGUI=False, color='white', unit='height')
 #%% your loop here
 # start by copying your one trial here, then identify what needs to be
 # changed on every trial.  Likely your stimuli, but you might want to change a few things
-
+ratingScale = visual.RatingScale(win, textColor = 'black', lineColor = 'black', choices=['cute', 'very cute', 'cutest'], pos=(0, -.45))
+text = visual.TextStim(win, text='How cute is this picture?', color = 'black', pos=(0,-.4), height=.05)
 
 # make a list or a pd.DataFrame that contains trial-specific info (stimulus, etc)
 # e.g. stim = ['1.jpg','2.jpg','3.jpg']
-
+imageList = ['Pics/Gizmo.jpg', 'Pics/Minnie.jpg', 'Pics/Artemis.jpg']
+data = []
 
 # make your loop
-for t in ... :
+for image in imageList:
+    x, y = ratingScale.win.size
+    text = visual.SimpleImageStim(win=win, image=image, units='pix', pos=[0, y//7])
+    ratingScale.reset()
+    event.clearEvents()
+    while ratingScale.noResponse:
+         text.draw()
+         ratingScale.draw()
+         win.flip()
+         if event.getKeys(['escape']):
+             core.quit()
+    data.append([image, ratingScale.getRating(), ratingScale.getRT()])
+
+
     
     # include your trial code in your loop but replace anything that should 
     # change on each trial with a variable that uses your iterater
@@ -42,10 +72,19 @@ for t in ... :
     # if you're recording responses, be sure to store your responses in a list
     # or DataFrame which also uses your iterater!
 
+close = visual.TextStim(win, color = 'black', height=.05, text="""Rachel Forbes PS5
+Step 2: Build a trial loop
+
+You have rated all my pets! 
+Press escape to quit. """)
+
+event.clearEvents()
+close.draw()
+win.flip()
+if 'escape' in event.waitKeys():
+    core.quit()
 
 #%% Required clean up
 # this cell will make sure that your window displays for a while and then 
 # closes properly
-
-core.wait(2)
 win.close()
